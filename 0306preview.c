1c98c5497980f09469c48c3d372aaf4a2aa1791c
Dongwon-tuna <61178312+Dongwon-tuna@users.noreply.github.com>
2024-03-06 11:43:37 +0900

    GCD , PrimeNum test

--- 0306preview.c
+++ 0306preview.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 
 
-//Find GCD by minus, mod methods.
+//Find GCD by minus, mod methods. And test it's prime num.
 int GCD_minus(int a,int b){
     int c=0;
+    printf("=========================Using minus method=========================\n");
     while (a>=b)
     {
+        
         a = a-b;
         printf("this is minus val:%d, %d\n",a,b);
         
@@ -26,12 +28,12 @@ int GCD_minus(int a,int b){
 }
 
 int GCD_mod(int a, int b){
+    printf("=========================Using mod method=========================\n");
     int c=0;
     while (a>=b)
     {
         a = a%b;
         printf("this is mod val:%d, %d\n",a,b);
-        
         if (b>a)
         {
             c =a;
@@ -48,6 +50,26 @@ int GCD_mod(int a, int b){
 
 }
 
+void primeTester(int a){
+    int cnt=0;
+    for (int i = 1; i < a+1; i++)
+    {
+        if (a%i == 0)
+        {
+            cnt = cnt+1;
+        }
+   
+    }
+    if (cnt == 2)
+        {
+            printf("its Prime Num\n");
+        }
+    else{
+        printf("its not\n");
+    }
+    
+
+}
 int main(void){
     int input1,input2;
     printf("Input two numbers:");
@@ -58,6 +80,10 @@ int main(void){
     int result2 = GCD_mod(input1,input2);
     printf("GCD_minus: %d\n",result1);
     printf("GCD_mod: %d\n",result2);
+    primeTester(input1);
+
+
+
     return 0;
 
 
